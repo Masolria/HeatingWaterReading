e5bb8f1c640911f01c4e1566e62ca442fa4222a9
Maria_solsol <mioro.sol@gmail.com>
2024-01-28 13:46:52 +0400

    javadoc commit

--- src/main/java/ru/masolria/hwr/in/User.java
+++ src/main/java/ru/masolria/hwr/in/User.java
@@ -10,26 +10,46 @@ public class User {
     /**Поле, хранящее в себе все показания счетчиков по месяцам*/
     private MeterReadings meterReadings;
 
-
+    /**
+     * Конструктор класса User. Создает нового пользователя с указанным лицевым счетом и паролем
+     * и инициализирует показания счетчиков.
+     *
+     * @param personalAccount Лицевой счёт пользователя
+     * @param password Пароль пользователя
+     */
     public User(String personalAccount, String password) {
         this.personalAccount = personalAccount;
         this.password = password;
         meterReadings = new MeterReadings();
     }
-
+    /**
+     * Выводит на экран показания счетчиков для данного пользователя.
+     */
     public void printReadings(){
         System.out.println("Показания счетчиков "+ personalAccount +":");
         System.out.println(meterReadings.toString());
     }
-
+    /**
+     * Возвращает лицевой счёт пользователя.
+     *
+     * @return Лицевой счёт пользователя
+     */
     public String getPersonalAccount() {
         return personalAccount;
     }
-
+    /**
+     * Возвращает пароль пользователя.
+     *
+     * @return Пароль пользователя
+     */
     public String getPassword() {
         return password;
     }
-
+    /**
+     * Возвращает объект, представляющий показания счетчиков данного пользователя.
+     *
+     * @return Объект, представляющий показания счетчиков
+     */
     public MeterReadings getMeterReadings() {
         return meterReadings;
     }
