d1d457f46762aab99bd99abcbd163ac879123828
Maria_solsol <mioro.sol@gmail.com>
2024-01-28 12:52:21 +0400

    javadoc commit

--- src/main/java/ru/masolria/hwr/in/MeterReadings.java
+++ src/main/java/ru/masolria/hwr/in/MeterReadings.java
@@ -7,36 +7,58 @@ import java.util.Calendar;
 
 public class MeterReadings {
 
-    /**Поле, содержащее месяца показаний*/
-    private ArrayList<Calendar> monthsOfReading = new ArrayList<>();
-    /**Поле, содержащее показания холодной воды*/
-    private ArrayList<Float> coldWater = new ArrayList<>();
-    /**Поле, содержащее показания горячей воды*/
-    private ArrayList<Float> hotWater = new ArrayList<>();
-    /**Поле, содержащее показания отопления*/
-    private ArrayList<Float> heating = new ArrayList<>();
     /**
-     * Возвращает 
+     * Поле, содержащее месяца показаний
+     */
+    private ArrayList<Calendar> monthsOfReading = new ArrayList<>();
+    /**
+     * Поле, содержащее показания холодной воды
+     */
+    private ArrayList<Float> coldWater = new ArrayList<>();
+    /**
+     * Поле, содержащее показания горячей воды
+     */
+    private ArrayList<Float> hotWater = new ArrayList<>();
+    /**
+     * Поле, содержащее показания отопления
+     */
+    private ArrayList<Float> heating = new ArrayList<>();
+
+    /**
+     * Проверяет, прошел ли месяц с прошлого добавления показаний.
      *
+     * @return true - если с прошлого добавления показаний прошел месяц, иначе - false.
+     * Если список пустой, возвращает true.
      */
     public boolean monthPassed() {
-        if(monthsOfReading.size() ==0){
+        if (monthsOfReading.size() == 0) {
             return true;
         }
         Calendar currentMonth = Calendar.getInstance();
         Calendar lastMonth = monthsOfReading.get(monthsOfReading.size() - 1);
-        int diffYear = currentMonth.get(Calendar.YEAR)-lastMonth.get(Calendar.YEAR);
+        int diffYear = currentMonth.get(Calendar.YEAR) - lastMonth.get(Calendar.YEAR);
         return diffYear >= 1 || (currentMonth.get(Calendar.MONTH) - lastMonth.get(Calendar.MONTH) != 0);
     }
 
-
+    /**
+     * Добавляет показания в список показаний.
+     * Если одно из показаний меньше предыдущего - возвращает false.
+     * Если с последнего показания не прошел месяц - возвращает false.
+     * Если показание меньше нуля - возвращает false.
+     *
+     * @param cold - показание холодной воды
+     * @param hot  - показание горячей воды
+     * @param heat - показание отопления
+     * @param time - время текущего добавления
+     * @return если добаление произошло успешно, иначе false.
+     */
     public boolean addReading(float cold, float hot, float heat, Calendar time) {
-        if (cold < 0 || hot < 0 || heat < 0||(!monthPassed())) return false;
+        if (cold < 0 || hot < 0 || heat < 0 || (!monthPassed())) return false;
         if (monthsOfReading.size() != 0) {
             float previousCold = coldWater.get(coldWater.size() - 1);
             float previousHot = hotWater.get(hotWater.size() - 1);
             float previousHeat = heating.get(heating.size() - 1);
-            if ( previousCold > cold || previousHot > hot
+            if (previousCold > cold || previousHot > hot
                     || previousHeat > heat) {
                 return false;
             }
@@ -49,6 +71,10 @@ public class MeterReadings {
         return true;
     }
 
+    /**
+     * Метод, который возвращает показания счетчиков с указанием вида показаний.
+     * @return строковое представление показаний счетчиков.
+     */
     @Override
     public String toString() {
         SimpleDateFormat formatter = new SimpleDateFormat("MM-yyyy");
