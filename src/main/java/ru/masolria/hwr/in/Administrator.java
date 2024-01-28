d210a9da3c929750337e1d73e0a912afe0e96829
Maria_solsol <mioro.sol@gmail.com>
2024-01-28 12:25:24 +0400

    javadoc commit

--- src/main/java/ru/masolria/hwr/in/Administrator.java
+++ src/main/java/ru/masolria/hwr/in/Administrator.java
@@ -2,12 +2,30 @@ package ru.masolria.hwr.in;
 
 import java.util.HashSet;
 
+/**
+ * Класс администратор, предоставляет функционал для задач админа,
+ * такие как отображение показаний счетчиков всех пользователей.
+ */
 public class Administrator {
+    /** Хранит в себе пароль для доступа к функционалу администратора.
+     */
     static private final String password = "AdminPassword";
+
+    /**
+     * Сравнивает предоставленный пароль с паролем администратора
+     * @param passwd сравниваемый пароль с паролем администратора
+     * @return true - если пароли совпадают, false - если нет.
+     */
     boolean checkPassword(String passwd){
         if (passwd.equals(password)) return true;
         return false;
     }
+
+    /**
+     * Выводит в консоль показания счетчиков всех пользователей
+     * с указанием номера лицевого счета пользователя.
+     * @param users набор пользователей для вывода в консоль
+     */
     public void printAllReadings(HashSet<User> users){
         for (User user : users) {
             System.out.println(user.getPersonalAccount() + ":\n");
